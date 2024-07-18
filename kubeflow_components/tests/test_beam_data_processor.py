eb77f4cfe3ede30e0d73e386fd7f3493eefcbfb8
sapphire008 <cui23327@gmail.com>
2024-07-17 22:11:40 -0400

    move test

--- kubeflow_components/tests/test_beam_data_processor.py
+++ kubeflow_components/tests/test_beam_data_processor.py
@@ -62,6 +62,40 @@ def test_csv_reader_writer():
 @pytest.mark.skip(
     reason="working already, skip for now during development"
 )
+def test_beam_data_processing_single_component():
+    """No input/output artifacts. Just static files linked"""
+    input_file = "kubeflow_components/tests/data/input.csv"
+    # Initialize the runner
+    runner = LocalPipelineRunner(runner="subprocess")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # output_file = os.path.join(temp_dir, "output")
+        # make payload
+        payload = {
+            "processing_fn": "kubeflow_components.tests.conftest.processing_fn",
+            "init_fn": "kubeflow_components.tests.conftest.init_fn",
+            "input_data": CsvInputData(
+                file=input_file, batch_size=2
+            ).as_dict(),
+            "output_data": CsvOutputData(
+                # file=output_file,
+                num_shards=3,
+                headers=["A", "B"],
+            ).as_dict(),
+        }
+        # Run the pipeline
+        task = runner.create_run(
+            beam_data_processing_component, payload
+        )
+
+        # Check that there are 3 files
+        files = os.listdir(task.output.uri)
+        assert len(files) == 3, "Expected 3 files"
+        shutil.rmtree(task.output.uri)  # clean up
+
+
+@pytest.mark.skip(
+    reason="working already, skip for now during development"
+)
 def test_bigquery_output_data():
     # Two modes of specifying BigQuery Output
     output_data1 = BigQueryOutputData(
@@ -141,35 +175,3 @@ def test_bigquery_reader_writer():
     )
 
 
-@pytest.mark.skip(
-    reason="working already, skip for now during development"
-)
-def test_beam_data_processing_single_component():
-    """No input/output artifacts. Just static files linked"""
-    input_file = "kubeflow_components/tests/data/input.csv"
-    # Initialize the runner
-    runner = LocalPipelineRunner(runner="subprocess")
-    with tempfile.TemporaryDirectory() as temp_dir:
-        # output_file = os.path.join(temp_dir, "output")
-        # make payload
-        payload = {
-            "processing_fn": "kubeflow_components.tests.conftest.processing_fn",
-            "init_fn": "kubeflow_components.tests.conftest.init_fn",
-            "input_data": CsvInputData(
-                file=input_file, batch_size=2
-            ).as_dict(),
-            "output_data": CsvOutputData(
-                # file=output_file,
-                num_shards=3,
-                headers=["A", "B"],
-            ).as_dict(),
-        }
-        # Run the pipeline
-        task = runner.create_run(
-            beam_data_processing_component, payload
-        )
-
-        # Check that there are 3 files
-        files = os.listdir(task.output.uri)
-        assert len(files) == 3, "Expected 3 files"
-        shutil.rmtree(task.output.uri)  # clean up
