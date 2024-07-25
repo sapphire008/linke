a6e9600727a3a34c82008f74c863932387dfd00b
sapphire008 <cui23327@gmail.com>
2024-07-24 22:46:56 -0400

    adding example model

---
+++ examples/masknet_recommender/model.py
@@ -0,0 +1,345 @@
+from typing import Dict, Literal, List, Optional
+from dataclasses import dataclass
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from pdb import set_trace
+
+
+@dataclass
+class FeatureSpec:
+    type: Literal["categorical", "numerical"]
+    embed_size: int
+    vocab_size: int = None  # only valid for categorical features
+    padding_idx: int = None  # only valid for categorical features
+    sequence_len: int = (
+        None  # non-null values indicate sequential features
+    )
+
+
+class FeatureEmbedding(nn.Module):
+    def __init__(self, feature_specs: Dict[str, FeatureSpec]):
+        super().__init__()
+        self.feature_specs = feature_specs
+        # Create embedding layers
+        self.embeddings = {}
+        self.embed_dim = 0
+        for feat, spec in feature_specs.items():
+            if spec.type == "categorical":
+                self.embeddings[feat] = nn.Embedding(
+                    spec.vocab_size + int(spec.padding_idx is not None),
+                    spec.embed_size,
+                    padding_idx=spec.padding_idx,
+                )
+            elif spec.type == "numerical":  # numerical
+                self.embeddings[feat] = nn.Linear(
+                    spec.sequence_len or 1, spec.embed_size
+                )
+            self.embed_dim += spec.embed_size
+
+    def forward(self, x):
+        # Get embeddings
+        v_embed = []
+        v_embed_ln = []  # layer_normalized embeddings
+        for feat, val in x.items():
+            if feat not in self.embeddings:
+                continue
+            # (batch_size, emb_size)
+            emb = self.embeddings[feat](val)
+
+            # Aggregate categorical sequence features
+            # In practice, this can insert other sequence models
+            # such as transformer to create sequence embedding
+            if (
+                self.feature_specs[feat].type == "categorical"
+                and self.feature_specs[feat].sequence_len is not None
+            ):
+                emb = torch.mean(emb, dim=1)
+
+            # Aggregate
+            v_embed.append(emb)
+            embed_dim = self.feature_specs[feat].embed_size
+            v_embed_ln.append(F.layer_norm(emb, (embed_dim,)))
+
+        v_embed = torch.cat(v_embed, dim=-1)
+        v_embed_ln = torch.cat(v_embed_ln, dim=-1)
+
+        return v_embed, v_embed_ln
+
+
+class InstanceGuidedMask(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        aggregation_dim: int,
+        projection_dim: Optional[int] = None,
+    ):
+        super().__init__()
+        self.aggregation_layer = nn.Linear(input_dim, aggregation_dim)
+        self.projection_layer = nn.Linear(
+            aggregation_dim, projection_dim or input_dim
+        )
+
+    def forward(self, x):
+        # x is embedding of shape (batch_size, seq_len, emb_size)
+        mask = self.aggregation_layer(x)
+        mask = F.relu(mask)
+        mask = self.projection_layer(mask)
+        return mask
+
+
+class MaskBlock(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        mask_hidden_dim: int,
+        mask_output_dim: Optional[int] = None,
+        output_dim: Optional[int] = None,
+    ):
+        """
+        MaskBlock. Can be used for both MaskBlock on Feature Embedding
+        and MaskBlock on MaskBlock
+
+        Parameters
+        ----------
+        input_dim : int
+            Input dimension of the block
+        mask_hidden_dim : int
+            instance guided mask hidden dimension
+        mask_output_dim: int
+            instance guided mask output dimension. If not specified,
+            returns a mask of the same size as input_dim
+        output_dim : int, optional
+            optionally specify an output dimension of the block.
+            Otherwise, the block will output features of the same size
+            as the input
+        """
+        super().__init__()
+        self.instance_guided_mask = InstanceGuidedMask(
+            input_dim, mask_hidden_dim, mask_output_dim
+        )
+        self.hidden = nn.Linear(
+            mask_output_dim or input_dim,
+            output_dim or input_dim,
+            bias=False,
+        )
+        self.layer_norm = nn.LayerNorm((output_dim or input_dim,))
+
+    def forward(self, x, output):
+        mask = self.instance_guided_mask(x)  # masked features
+
+        # Apply mask:
+        # output: previous maskblock output,
+        # or individually layer normalized embeddings
+        y = mask * output
+        y = self.hidden(y)
+
+        # Output hidden
+        y = self.layer_norm(y)
+        y = F.relu(y)
+
+        return y
+
+
+class MaskNet(nn.Module):
+    """
+    MaskNet implementation for both serial and parallel architecture.
+
+    Parameters
+    ----------
+    feature_specs : Dict[str, FeatureSpec]
+        Dictionary of feature specs.
+    num_blocks : int, optional
+        Number of MaskBlocks, by default 3
+    block_output_size : int, optional
+        Size of output for each block, by default 128
+    mask_hidden_dim : int, optional
+        Hidden or aggregation layer size of the mask, 
+        by default 256
+    architecture : Literal['serial', 'parallel'], optional
+        MaskNet architecture, by default "serial"
+    parallel_projection_sizes : List[int], optional
+        This is only relevant for parallel model architecture,
+        specifying the hidden layer sizes before output,
+        by default [128, 128]
+    """
+    def __init__(
+        self,
+        feature_specs: Dict[str, FeatureSpec],
+        num_blocks: int = 3,
+        block_output_size: int = 128,
+        mask_hidden_dim: int = 256,
+        architecture: Literal["serial", "parallel"] = "serial",
+        parallel_projection_sizes: List[int] = [128, 128],
+    ):
+        super().__init__()
+        self.architecture = architecture
+        # Initialize the embedding layers for features
+        self.embedding_layer = FeatureEmbedding(feature_specs)
+
+        # Create layers depending on architecture
+        if architecture == "parallel":
+            self.initialize_parallel_layers(
+                num_blocks,
+                mask_hidden_dim,
+                block_output_size,
+                parallel_projection_sizes,
+            )
+        else:  # serial
+            self.initialize_serial_layers(
+                num_blocks, mask_hidden_dim, block_output_size
+            )
+
+    def initialize_parallel_layers(
+        self,
+        num_blocks: int,
+        mask_hidden_dim: int,
+        block_output_size: int,
+        parallel_projection_sizes: List[int],
+    ):
+        """Create layers for parallel architecture."""
+        # Mask blocks
+        self.mask_blocks = []
+        for _ in range(num_blocks):
+            block = MaskBlock(
+                input_dim=self.embedding_layer.embed_dim,
+                mask_hidden_dim=mask_hidden_dim,
+                # parallel mask.shape == feature.shape
+                mask_output_dim=None,
+                output_dim=block_output_size,
+            )
+            self.mask_blocks.append(block)
+
+        # Output projection layers
+        hidden_size = (
+            block_output_size * num_blocks
+            if block_output_size
+            else self.embedding_layer.embed_dim * num_blocks
+        )
+        parallel_projection_sizes = [
+            hidden_size
+        ] + parallel_projection_sizes
+
+        self.output_projection = nn.Sequential()
+        for ii in range(len(parallel_projection_sizes) - 1):
+            f"parallel_output_projection_{ii}"
+            self.output_projection.add_module(
+                f"parallel_output_projection_{ii}",
+                nn.Linear(
+                    parallel_projection_sizes[ii],
+                    parallel_projection_sizes[ii + 1],
+                ),
+            )
+        # Output projection
+        self.prediction_layer = nn.Linear(
+            parallel_projection_sizes[-1], 1
+        )
+
+    def initialize_serial_layers(
+        self,
+        num_blocks: int,
+        mask_hidden_dim: int,
+        block_output_size: int = None,
+    ):
+        """Create layers for serial architecture."""
+        # Mask blocks
+        self.mask_blocks = []
+        for ii in range(num_blocks):
+            block = MaskBlock(
+                input_dim=self.embedding_layer.embed_dim,
+                mask_hidden_dim=mask_hidden_dim,
+                # First block is masking on the feature, so the mask
+                # size needs to be the same as the input
+                # the rest of the layers follows block_output_size
+                mask_output_dim=None if ii == 0 else block_output_size,
+                output_dim=block_output_size,
+            )
+            self.mask_blocks.append(block)
+
+        # Output projection
+        self.prediction_layer = nn.Linear(
+            block_output_size or self.embedding_layer.embed_dim, 1
+        )
+
+    def forward(self, x, y=None):
+        v_embed, v_embed_ln = self.embedding_layer(x)
+        if self.architecture == "serial":
+            z = self._forward_serial(v_embed, v_embed_ln)
+        elif self.architecture == "parallel":
+            z = self._forward_parallel(v_embed, v_embed_ln)
+        z = self.prediction_layer(z)
+
+        return z
+
+    def _forward_parallel(self, v_embed, v_embed_ln):
+        hidden = []
+        for block in self.mask_blocks:
+            hidden.append(block(v_embed, v_embed_ln))
+        hidden = torch.cat(hidden, dim=-1)
+        projection = self.output_projection(hidden)
+        return projection
+
+    def _forward_serial(self, v_embed, v_embed_ln):
+        mask_block_output = v_embed_ln
+        for block in self.mask_blocks:
+            mask_block_output = block(v_embed, mask_block_output)
+        return mask_block_output
+
+
+if __name__ == "__main__":
+    batch_size = 4
+    seq_len = 50
+    feature_specs = {
+        "day_of_week": FeatureSpec(
+            type="categorical",
+            embed_size=10,
+            vocab_size=7,
+            padding_idx=0,
+        ),
+        "hour_of_day": FeatureSpec(
+            type="categorical", embed_size=10, vocab_size=24
+        ),
+        "account_tenure": FeatureSpec(
+            type="categorical",
+            embed_size=10,
+            vocab_size=5,
+            padding_idx=0,
+        ),
+        "payment_tier": FeatureSpec(
+            type="categorical",
+            embed_size=10,
+            vocab_size=3,
+            padding_idx=0,
+        ),
+        "watch_history": FeatureSpec(
+            type="categorical",
+            embed_size=64,
+            vocab_size=500,
+            sequence_len=50,
+            padding_idx=0,
+        ),
+        "percent_watched": FeatureSpec(
+            type="numerical", embed_size=10, sequence_len=50
+        ),
+    }
+
+    x = {
+        "day_of_week": torch.randint(1, 8, (batch_size,)),
+        "hour_of_day": torch.randint(0, 24, (batch_size,)),
+        "account_tenure": torch.randint(1, 6, (batch_size,)),
+        "payment_tier": torch.randint(1, 4, (batch_size,)),
+        "watch_history": torch.randint(1, 501, (batch_size, seq_len)),
+        "percent_watched": torch.rand((batch_size, seq_len)),
+    }
+
+    model = MaskNet(
+        feature_specs,
+        num_blocks=3,
+        block_output_size=17,
+        architecture="serial",
+    )
+
+    model.eval()
+    with torch.no_grad():
+        output = model(x)
--- kubeflow_components/serving/pusher.py
+++ kubeflow_components/serving/pusher.py
@@ -7,64 +7,85 @@ from kubernetes.stream import stream
 import tempfile
 import os
 
-# Load the Kubernetes configuration
-config.load_kube_config()  # For local kubectl config
-# or config.load_incluster_config()  # If running inside a pod
 
-# Create API client
-v1 = client.CoreV1Api()
+class KubernetesPusher:
+    def __init__(
+        self,
+        namespace="default",
+        pod_name="remote-model-store-pod",
+        remote_kube_config_path: str = None,
+    ):
+        # Load the Kubernetes configuration
+        if remote_kube_config_path:
+            config.load_kube_config(
+                remote_kube_config_path
+            )  # For local kubectl config
+        else:
+            try:
+                config.load_kube_config()  # For local kubectl config
+            except:
+                config.load_incluster_config()  # If running inside a pod
 
-# Define the pod and namespace
-pod_name = "model-store-pod"
-namespace = (
-    "default"  # Change this if your pod is in a different namespace
-)
+        # Create API client
+        self.v1 = client.CoreV1Api()
+
+        # Define the pod and namespace
+        self.pod_name = pod_name
+        self.namespace = namespace
+
+    def exec_command(self, command):
+        return stream(
+            self.v1.connect_get_namespaced_pod_exec,
+            self.pod_name,
+            self.namespace,
+            command=command,
+            stderr=True,
+            stdin=False,
+            stdout=True,
+            tty=False,
+        )
+
+    # Function to copy file to pod
+    def copy_file_to_pod(self, local_path, pod_path):
+        with open(local_path, "rb") as file:
+            data = file.read()
+
+        self.exec_command(["mkdir", "-p", os.path.dirname(pod_path)])
+
+        with tempfile.NamedTemporaryFile() as temp_file:
+            temp_file.write(data)
+            temp_file.flush()
+
+            client.CoreV1Api().read_namespaced_pod(
+                name=self.pod_name, namespace=self.namespace
+            )
+
+            self.exec_command(
+                ["cp", "/dev/stdin", pod_path],
+                stdin=open(temp_file.name, "rb"),
+            )
 
 
-def exec_command(command):
-    return stream(
-        v1.connect_get_namespaced_pod_exec,
-        pod_name,
-        namespace,
-        command=command,
-        stderr=True,
-        stdin=False,
-        stdout=True,
-        tty=False,
-    )
+class StoragePusher:
+    def __init__(self, client):
+        pass
+
+    def push(self):
+        pass
 
 
+kubernetes_pusher = KubernetesPusher()
 # Create directories
-exec_command(["mkdir", "-p", "/pv/model-store/"])
-exec_command(["mkdir", "-p", "/pv/config/"])
-
-
-# Function to copy file to pod
-def copy_file_to_pod(local_path, pod_path):
-    with open(local_path, "rb") as file:
-        data = file.read()
-
-    exec_command(["mkdir", "-p", os.path.dirname(pod_path)])
-
-    with tempfile.NamedTemporaryFile() as temp_file:
-        temp_file.write(data)
-        temp_file.flush()
-
-        client.CoreV1Api().read_namespaced_pod(
-            name=pod_name, namespace=namespace
-        )
-
-        exec_command(
-            ["cp", "/dev/stdin", pod_path],
-            stdin=open(temp_file.name, "rb"),
-        )
-
+kubernetes_pusher.exec_command(["mkdir", "-p", "/pv/model-store/"])
+kubernetes_pusher.exec_command(["mkdir", "-p", "/pv/config/"])
 
 # Copy files
-copy_file_to_pod(
+kubernetes_pusher.copy_file_to_pod(
     "squeezenet1_1.mar", "/pv/model-store/squeezenet1_1.mar"
 )
-copy_file_to_pod("mnist.mar", "/pv/model-store/mnist.mar")
-copy_file_to_pod("config.properties", "/pv/config/config.properties")
-
-print("Files copied successfully.")
+kubernetes_pusher.copy_file_to_pod(
+    "mnist.mar", "/pv/model-store/mnist.mar"
+)
+kubernetes_pusher.copy_file_to_pod(
+    "config.properties", "/pv/config/config.properties"
+)
