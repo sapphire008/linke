b4ba58358b56681f2f1019b993ca5e2ba6289ebf
Edward Cui <cui23327@gmail.com>
2024-09-09 10:43:10 -0400

    Update evaluator.py
    Add necessary components for evaluation

--- kubeflow_components/evaluation/evaluator.py
+++ kubeflow_components/evaluation/evaluator.py
@@ -1,17 +1,65 @@
 import json
-from typing import List, Dict, Tuple, Optional
+from dataclasses import dataclass
+from typing import List, Optional
 import apache_beam as beam
 
-from kubeflow_components.dataset.beam_data_processor import (
-    ReadBigQueryData,
-    ReadTFRecordData,
-)
 
-# Read Data
-# Make Inference
-# %% Write metric file
+# %% TFMA-like Model Eval without Protobuf
+# and use models in addition to Tensorflow
+
+@dataclass
+class ModelSpec:
+    pass
+
+@dataclass
+class MetricSpec:
+    pass
 
 
+class MetricConfig:
+    def __init__(self):
+        pass
+
+class SliceConfig:
+    def __init__(self):
+        pass
+
+@dataclass
+class DataSpec:
+    input_data: List
+    slices: List[SliceConfig] = [SliceConfig()]
+
+
+class EvalConfig:
+    def __init__(
+        self,
+        model: ModelSpec,
+        metrics: List[MetricSpec],
+        data: DataSpec,
+        slices: List[SliceConfig] = [SliceConfig()],
+        baseline_model: Optional[ModelSpec] = None,
+    ):
+        """
+        Evaluation Configuration.
+
+        Args:
+            model (ModelSpec): model configuration
+            metrics (List[MetricSpec]): List of metrics to compute
+            data (DataSpec): evaluation data configuration
+            baseline_model (Optional[ModelSpec], optional): baseline
+                model to compare to. This is useful to calculate
+                metrics that compares baseline models, such as
+                the difference in the set of predictions. Defaults to 
+                None.
+        """
+        self.model = model
+        self.data = data
+        self.baseline_model = baseline_model
+        self.metrics = metrics
+        self.slices = slices
+
+
+# %% Metric writer
 class MetricWriter(beam.PTransform):
     def __init__(self, output_file):
         self.output_file = output_file
@@ -22,3 +70,13 @@ class MetricWriter(beam.PTransform):
             | beam.Map(lambda x: json.dumps(x))
             | beam.io.WriteToText(self.output_file)
         )
+
+
+# %% Model prediction
+class ModelInference(beam.PTransform):
+    """Batch job to make inference on model."""
+    def __init__(self):
+        pass
+
+    def expand(self, pcoll):
+        return pcoll
