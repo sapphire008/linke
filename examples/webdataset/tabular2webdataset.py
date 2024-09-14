2a3c7e16f1a141f1fc28a227314bd5418596cc57
sapphire008 <cui23327@gmail.com>
2024-09-14 14:11:25 -0400

    correcting remaining pacakge names

--- examples/webdataset/tabular2webdataset.py
+++ examples/webdataset/tabular2webdataset.py
@@ -56,7 +56,7 @@ for s in tabular_dataset:
 #%%
 
 tabular_dataset = (
-    wds.WebDataset("/Users/edward/Documents/Scripts/kubeflow-components/kubeflow_components/dataset/archive_fileio/")
+    wds.WebDataset("/Users/edward/Documents/Scripts/linke/linke/dataset/archive_fileio/")
     .to_tuple("json").batched(2)
 )
 for s in tabular_dataset:
